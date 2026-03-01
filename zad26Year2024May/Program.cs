a8c2f48796e6db81c507251ce5c4aac68f6fa505
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-03-01 14:47:55 +0200

    code cleanup

--- zad26Year2024May/Program.cs
+++ zad26Year2024May/Program.cs
@@ -1,36 +1,48 @@
 ﻿
-
-var a = int.Parse(Console.ReadLine());
-var b = int.Parse(Console.ReadLine());
-
-var digitCountDic = new Dictionary<int, int>();
-
-for (int i = a; i <= b; i++)
+try
 {
-    int currNum = i;
+    var a = int.Parse(Console.ReadLine()!);
+    var b = int.Parse(Console.ReadLine()!);
 
-    while (currNum != 0)
+    if (a < 0 || b < 0 || a > b || b > 1000000)
     {
-        int digit = currNum % 10;
-        if (digitCountDic.ContainsKey(digit))
-        {
-            digitCountDic[digit]++;
-        }
-        else
-        {
-            digitCountDic.Add(digit, 1);
-        }
-        currNum/= 10;
+        throw new ArgumentException("Invalid input");
     }
+
+    var digitCountDic = new Dictionary<int, int>();
+
+    for (int i = a; i <= b; i++)
+    {
+        int currNum = i;
+
+        while (currNum != 0)
+        {
+            int digit = currNum % 10;
+            if (digitCountDic.ContainsKey(digit))
+            {
+                digitCountDic[digit]++;
+            }
+            else
+            {
+                digitCountDic.Add(digit, 1);
+            }
+            currNum /= 10;
+        }
+    }
+
+    var minKey = digitCountDic
+        .OrderByDescending(x => x.Value)
+        .ThenBy(x => x.Key)
+        .FirstOrDefault();
+
+    Console.WriteLine($"Digit {minKey.Key} - {minKey.Value} times");
+
+
 }
-
-var minKey = digitCountDic
-    .OrderByDescending(x => x.Value)
-    .ThenBy(x => x.Key)
-    .FirstOrDefault();
-
-Console.WriteLine($"Digit {minKey.Key} - {minKey.Value} times");
-
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
 
 
 
