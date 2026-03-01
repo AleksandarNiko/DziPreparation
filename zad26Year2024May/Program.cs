6b9a99e0d3394da8109996699d6f88817bc02a7a
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-03-01 14:38:27 +0200

    2024 completed

--- zad26Year2024May/Program.cs
+++ zad26Year2024May/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+var a = int.Parse(Console.ReadLine());
+var b = int.Parse(Console.ReadLine());
+
+var digitCountDic = new Dictionary<int, int>();
+
+for (int i = a; i <= b; i++)
+{
+    int currNum = i;
+
+    while (currNum != 0)
+    {
+        int digit = currNum % 10;
+        if (digitCountDic.ContainsKey(digit))
+        {
+            digitCountDic[digit]++;
+        }
+        else
+        {
+            digitCountDic.Add(digit, 1);
+        }
+        currNum/= 10;
+    }
+}
+
+var minKey = digitCountDic
+    .OrderByDescending(x => x.Value)
+    .ThenBy(x => x.Key)
+    .FirstOrDefault();
+
+Console.WriteLine($"Digit {minKey.Key} - {minKey.Value} times");
+
+
+
+
+
