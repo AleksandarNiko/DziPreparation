b3e3e416ed26b5a262cec549a3e01eff90755ace
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-05-03 17:20:09 +0300

    some code

---
+++ 01.GetDayByDate/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int[] dayMonth=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int day = dayMonth[0];
+int month = dayMonth[1];
+
+DateTime date = new DateTime(2026, month, day);
+
+Console.WriteLine(date.DayOfWeek);
