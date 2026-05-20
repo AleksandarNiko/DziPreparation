9890e2ce884e758d4582ff097f8f72b1009ac91e
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-05-20 17:26:46 +0300

    last chance

---
+++ 01.SoftwareCatalogue/App.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.SoftwareCatalogue
+{
+    public class App
+    {
+        private string name;
+        private string category;
+        private double rating;
+        private int downloads;
+        private decimal price;
+
+        public string Name { get => name; set 
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                name = value;
+                
+            } 
+        }
+
+        public string Category { get => category; set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                category = value;
+
+            }
+        }
+
+        public double Rating { get => rating; set 
+            {
+                if(value < 1 || value > 5)
+                {
+                    throw new ArgumentException("Rating must be between 0 and 5.");
+                }
+                rating = value;
+            } 
+        }
+
+        public int Downloads { get => downloads; set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Downloads must be a positive number.");
+                }
+                downloads = value;
+            }
+        }
+
+        public decimal Price { get => price; set => price = value; }
+    }
+}
---
+++ 01.SoftwareCatalogue/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using _01.SoftwareCatalogue;
+
+int appsCount = int.Parse(Console.ReadLine());
+var apps = new List<App>();
+
+for (int i = 0; i < appsCount; i++)
+{
+    string[] appInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string name = appInfo[0];
+    string category = appInfo[1];
+    double rating = double.Parse(appInfo[2]);
+    int downloads = int.Parse(appInfo[3]);
+    decimal price = decimal.Parse(appInfo[4]);
+    App app = new App
+    {
+        Name = name,
+        Category = category,
+        Rating = rating,
+        Downloads = downloads,
+        Price = price
+    };
+    apps.Add(app);
+}
+
+string categoryToSearch = Console.ReadLine();
+
+var filteredApps = apps.Where(a => a.Category == categoryToSearch)
+    .OrderByDescending(a => a.Rating)
+    .ThenByDescending(a => a.Downloads)
+    .ToList();
+
+var downloadsSum = filteredApps.Sum(a => a.Downloads);
+
+if (filteredApps.Any())
+{
+    foreach (var app in filteredApps)
+    {
+        Console.WriteLine($"{app.Name} - Рейтинг: {app.Rating:f1}");
+    }
+    Console.WriteLine($"Общ брой изтегляния: {downloadsSum}");
+}
+else
+{
+    Console.WriteLine($"Няма намерени приложения");
+}
+
+/*
+4
+Duolingo Образование 4,7 500000 0,0
+Minecraft Игри 4,8 1000000 12,99
+Anki Образование 4,5 50000 0,0
+Coursera Образование 4,7 200000 0,0
+Образование
+*/
