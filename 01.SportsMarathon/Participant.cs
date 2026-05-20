585dca867a22831b649e30f53dc9a2648542c449
AleksandarNiko <145496882+AleksandarNiko@users.noreply.github.com>
2026-05-20 17:38:03 +0300

    one more

---
+++ 01.SportsMarathon/Participant.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _01.SportsMarathon
+{
+    public class Participant
+    {
+        private string name;
+        private string country;
+        private string ageGroup;
+        private int time;
+
+        public string Name { get => name; set 
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                name = value;
+            } 
+        }
+
+        public string Country { get => country; set 
+            {
+                if(string.IsNullOrEmpty(value) || value.Length!=3)
+                {
+                    throw new ArgumentException("Country must be a valid 3-letter code.");
+                }
+                country = value;
+            } }
+
+        public string AgeGroup { get => ageGroup; set 
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Age group cannot be null or empty.");
+                } ageGroup = value;
+            } }
+
+        public int Time { get => time; set 
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Time cannot be negative.");
+                } time = value;
+            } }
+
+
+    }
+}
---
+++ 01.SportsMarathon/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using _01.SportsMarathon;
+
+int playersCount = int.Parse(Console.ReadLine());
+
+var participants = new List<Participant>();
+
+for (int i = 0; i < playersCount; i++)
+{
+    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    Participant participant = new Participant
+    {
+        Name = input[0],
+        Country = input[1],
+        AgeGroup = input[2],
+        Time = int.Parse(input[3])
+    };
+    participants.Add(participant);
+}
+
+string ageGroupToCheck = Console.ReadLine();
+int maxTime = int.Parse(Console.ReadLine());
+
+var filteredParticipants = participants
+    .Where(p => p.AgeGroup == ageGroupToCheck && p.Time <= maxTime)
+    .OrderBy(p => p.Time)
+    .ToList();
+
+if (filteredParticipants.Any())
+{
+    foreach (var participant in filteredParticipants)
+    {
+        Console.WriteLine($"{participant.Name} {participant.Country} - {participant.Time} сек.");
+    }
+
+    Console.WriteLine("Средно време: {0:F2} сек.", filteredParticipants.Average(p => p.Time));
+
+}
+else
+{
+    Console.WriteLine("No participants found.");
+}
+
+/*
+4
+Иван BUL Мъже 3600
+Джон USA Мъже 3450
+Пиер FRA Ветерани 4200
+Алекс BUL Мъже 3800
+Мъже
+3700
+*/
